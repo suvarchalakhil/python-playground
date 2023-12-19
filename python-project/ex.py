c9ada3e2809fe5e61df29e400bffc7fa24b7aba3
toEpam <toepammiddle@gmail.com>
2023-12-19 18:01:32 +0500

    .

--- python-project/ex.py
+++ python-project/ex.py
@@ -1,12 +1,2 @@
 class Solution:
     def merge_dictionaries(self, *dicts: list[dict[int, int]]) -> dict[int, int]:
-        """
-        :type dicts: list[dict[int, int]]
-        :rtype: dict[int, int]
-        """
-        return {**dicts}
-dic1 = {1: 10, 2: 20}
-dic2 = {3: 30, 4: 40}
-dic3 = {5: 50, 6: 60}
-print("\n================================")
-print(Solution().merge_dictionaries(dic1, dic2, dic3))
